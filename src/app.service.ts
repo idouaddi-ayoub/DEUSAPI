cd72022d18607c880fcb23fe4780279b7635c238
idouaddi-ayoub <113615894+idouaddi-ayoub@users.noreply.github.com>
2023-08-20 20:34:56 +0100

    I have no idea what i'm doing

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,16 +1,18 @@
-import { Neo4jService } from "./neo4j/neo4j.service";
-import { Injectable } from "@nestjs/common" 
+import { Neo4jService } from './neo4j/neo4j.service';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
+export class AppService {
+  constructor(private readonly neo4jservice: Neo4jService) {}
 
-export class AppService{
-    constructor(private readonly neo4jservice: Neo4jService){}
+  async getHello(): Promise<string> {
+    const result = await this.neo4jservice.read(
+      `MATCH (n) RETURN count(n) AS count`,
+      {},
+    );
 
-    async getHello(): Promise<string> {
-        const result = await this.neo4jservice.read(`MATCH (n) RETURN count(n) AS count`, {})
+    const count = result.records[0].get('count');
 
-        const count = result.records[0].get('count')
-
-        return `Hello Neo4j User! The are ${count} nodes in the database`;
-    }
+    return `Hello Neo4j User! The are ${count} nodes in the database`;
+  }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
-import { UserService } from '../user/user.service';
+import { User, UserService } from '../user/user.service';
 import { Neo4jService } from '../neo4j/neo4j.service';
 import { EncryptionService } from '../encryption/encryption.service';
 import { ConfigModule, ConfigService } from '@nestjs/config/dist';
@@ -12,6 +12,7 @@ import { JwtStrategy } from '../jwt/jwt.strategy';
 import { EncryptionModule } from '../encryption/encryption.module';
 import { MembershipModule } from '../membership/membership.module';
 import { PassportModule } from '@nestjs/passport';
+import { Record } from 'neo4j-driver';
 
 @Module({
   imports: [
@@ -47,4 +48,21 @@ import { PassportModule } from '@nestjs/passport';
 })
 export class AuthModule {
   constructor(private readonly neo4jService: Neo4jService) {}
+
+  // onModuleInit() {
+  //   return Promise.all([
+  //     this.neo4jService
+  //       .write(
+  //         'CREATE CONSTRAINT ON (u:User) ASSERT u.id IS UNIQUE',
+  //         Record<User>,
+  //       )
+  //       .catch((e) => {}),
+  //     this.neo4jService
+  //       .write(
+  //         'CREATE CONSTRAINT ON (u:User) ASSERT u.email IS UNIQUE',
+  //         Record<User>,
+  //       )
+  //       .catch((e) => {}),
+  //   ]);
+  // }
 }
