4c4ee2685792951ad733f77b5e0e9049665fc2bc
idouaddi-ayoub <113615894+idouaddi-ayoub@users.noreply.github.com>
2023-08-20 20:18:10 +0100

    Creating User files

---
+++ src/user/user-claims.interface.ts
@@ -0,0 +1,4 @@
+export interface UserClaims {
+  email: string;
+  username: string;
+}
---
+++ src/user/user-properties.interface.ts
@@ -0,0 +1,6 @@
+export interface UserProperties {
+  email: string;
+  username: string;
+  password: string;
+  dateOfBirth: string;
+}
